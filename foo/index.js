3666d6b794d35b1dfef07e864af6370087ae699b
briankopp <briankopp.usa@gmail.com>
2019-06-15 13:44:09 -0600

    Update index file to use dotenv package when not in production

--- foo/index.js
+++ foo/index.js
@@ -1,4 +1,8 @@
 const app = require('./app');
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('express app listening on port 3000'));
+app.listen(port, () => console.log('express app listening on port ' + port));
